bc4936fbb43a5faebbc2472ec175eaa3becbb10b
Paul K George <paulkottalil20@gmail.com>
2020-09-21 00:51:16 +0530

    added react cookie and link to sign in to sp

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,37 @@ import AddStudent from './components/AddStudent';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import StudentDetail from './components/StudentDetail';
 
+import {useCookies} from 'react-cookie'
+
 
 
 function App() {
+
+  const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+
+  setCookie('csrftoken','7IQU6M03nvFmdRVNqhyhNm3HfA7y9TdbKmlzaZ2LaxrUxspdRMRiWEKz8FH7ITRL')
+  setCookie('sessionid', '44nvhbo77csq1n2tfqzta8bb18h0m1p7')
+
   return (
     <Router>
       <StudentProvider>
         
         <div className="App">
+          
           <Route path="/" exact>
             <NavLink to='/addStudent/'>
               <button type="button">
                 Add Student
               </button>
             </NavLink>
+            <br></br>
+            <a href="http://localhost:9000/idp/sso/init/?sp=http://localhost:8000/saml2/metadata/&RelayState=/">
+              SIgn in to SP
+            </a>
             <StudentList />
           </Route>
+
+          
           <Route path="/addStudent/" exact>
             <AddStudent />
           </Route>
